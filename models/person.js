bbe6f056134a7d01f7a59fbde20e07374f963dc1
Micaladan <55629315+Micaladan@users.noreply.github.com>
2021-01-10 17:36:14 -0500

    cluster map single dot links to show page for people

--- models/person.js
+++ models/person.js
@@ -1,22 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const PersonSchema = new Schema({
-  name: String,
-  image: String,
-  geometry: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const PersonSchema = new Schema(
+  {
+    name: String,
+    image: String,
+    geometry: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
+    description: String,
+    location: String,
   },
-  description: String,
-  location: String,
+  opts
+);
+
+PersonSchema.virtual('properties.popUpMarkup').get(function () {
+  return `<a href="/people/${this._id}">${this.name}</a>`;
 });
 
 module.exports = mongoose.model('Person', PersonSchema);
--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -63,8 +63,8 @@ map.on('load', function () {
     filter: ['!', ['has', 'point_count']],
     paint: {
       'circle-color': '#11b4da',
-      'circle-radius': 4,
-      'circle-stroke-width': 1,
+      'circle-radius': 11,
+      'circle-stroke-width': 2,
       'circle-stroke-color': '#fff',
     },
   });
@@ -92,15 +92,17 @@ map.on('load', function () {
   // the location of the feature, with
   // description HTML from its properties.
   map.on('click', 'unclustered-point', function (e) {
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var mag = e.features[0].properties.mag;
-    var tsunami;
+    const { popUpMarkup } = e.features[0].properties;
+    const coordinates = e.features[0].geometry.coordinates.slice();
 
-    if (e.features[0].properties.tsunami === 1) {
-      tsunami = 'yes';
-    } else {
-      tsunami = 'no';
-    }
+    // var mag = e.features[0].properties.mag;
+    // var tsunami;
+
+    // if (e.features[0].properties.tsunami === 1) {
+    //   tsunami = 'yes';
+    // } else {
+    //   tsunami = 'no';
+    // }
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -109,10 +111,7 @@ map.on('load', function () {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML('magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami)
-      .addTo(map);
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
   });
 
   map.on('mouseenter', 'clusters', function () {
